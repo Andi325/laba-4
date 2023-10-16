9a10e228773fd5171854e5ca732bd4f1f6a0a537
Andi345 <andreyivanichenko77@gmail.com>
2023-10-16 16:52:48 +0300

    first commit

---
+++ Система для моделювання та оптимізація трафіка у місті/3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Road
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+    public int NumberOfLanes { get; set; }
+    public int TrafficLevel { get; set; }
+}
+
+public class Vehicle : IDriveable
+{
+    public string Type { get; set; }
+    public double Speed { get; set; }
+    public double Size { get; set; }
+
+    public void Move()
+    {
+        Console.WriteLine($"The {Type} is moving at a speed of {Speed} km/h.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine($"The {Type} has stopped.");
+    }
+}
+
+public interface IDriveable
+{
+    void Move();
+    void Stop();
+}
+
+public class TrafficSimulation
+{
+    public static void SimulateTraffic(Road road, List<Vehicle> vehicles)
+    {
+        Console.WriteLine($"Simulating traffic on a {road.Length} km road with {road.NumberOfLanes} lanes...");
+
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.Move();
+        }
+
+        Console.WriteLine($"Traffic level: {road.TrafficLevel}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Road road = new Road
+        {
+            Length = 10,
+            Width = 3.5,
+            NumberOfLanes = 2,
+            TrafficLevel = 3
+        };
+
+        List<Vehicle> vehicles = new List<Vehicle>
+        {
+            new Vehicle { Type = "Car", Speed = 60, Size = 4 },
+            new Vehicle { Type = "Truck", Speed = 50, Size = 8 },
+            new Vehicle { Type = "Bus", Speed = 55, Size = 12 }
+        };
+
+        TrafficSimulation.SimulateTraffic(road, vehicles);
+    }
+}
---
+++ Умовна комютерна мережа/2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Computer
+{
+    public string IPAddress { get; set; }
+    public int Power { get; set; }
+    public string OS { get; set; }
+}
+
+public class Server : Computer
+{
+    public int StorageCapacity { get; set; }
+}
+
+public class Workstation : Computer
+{
+    public int RAMSize { get; set; }
+}
+
+public class Router : Computer
+{
+    public int Bandwidth { get; set; }
+}
+
+public interface IConnectable
+{
+    void Connect(Computer device);
+    void Disconnect(Computer device);
+    void SendData(Computer device, string data);
+    string ReceiveData(Computer device);
+}
+
+public class Network : IConnectable
+{
+    private Dictionary<string, Computer> devices = new Dictionary<string, Computer>();
+
+    public void Connect(Computer device)
+    {
+        devices.Add(device.IPAddress, device);
+        Console.WriteLine($"Connected: {device.IPAddress}");
+    }
+
+    public void Disconnect(Computer device)
+    {
+        devices.Remove(device.IPAddress);
+        Console.WriteLine($"Disconnected: {device.IPAddress}");
+    }
+
+    public void SendData(Computer device, string data)
+    {
+        if (devices.ContainsKey(device.IPAddress))
+        {
+            Console.WriteLine($"Sent data to {device.IPAddress}: {data}");
+        }
+        else
+        {
+            Console.WriteLine($"Error: {device.IPAddress} not found in the network.");
+        }
+    }
+
+    public string ReceiveData(Computer device)
+    {
+        if (devices.ContainsKey(device.IPAddress))
+        {
+            return $"Received data from {device.IPAddress}";
+        }
+        else
+        {
+            return $"Error: {device.IPAddress} not found in the network.";
+        }
+    }
+}
+
+class NetworkSimulation
+{
+    static void Main()
+    {
+        Network network = new Network();
+
+        Server server = new Server { IPAddress = "192.168.1.1", Power = 100, OS = "Windows", StorageCapacity = 1000 };
+        Workstation workstation = new Workstation { IPAddress = "192.168.1.2", Power = 50, OS = "Linux", RAMSize = 16 };
+        Router router = new Router { IPAddress = "192.168.1.3", Power = 80, OS = "RouterOS", Bandwidth = 100 };
+
+        network.Connect(server);
+        network.Connect(workstation);
+        network.Connect(router);
+
+        network.SendData(workstation, "Hello, server!");
+        network.SendData(router, "Routing data...");
+
+        string receivedData = network.ReceiveData(server);
+        Console.WriteLine(receivedData);
+
+        network.Disconnect(router);
+
+        receivedData = network.ReceiveData(router);
+        Console.WriteLine(receivedData);
+    }
+}
+
+
